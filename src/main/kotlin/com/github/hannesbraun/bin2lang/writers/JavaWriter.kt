49e9dab55bf67b30ffae0123cad31128de6ea0c1
Hannes Braun <hannesbraun@mail.de>
2021-04-02 12:27:15 +0200

    Fix JavaWriter

--- src/main/kotlin/com/github/hannesbraun/bin2lang/writers/JavaWriter.kt
+++ src/main/kotlin/com/github/hannesbraun/bin2lang/writers/JavaWriter.kt
@@ -5,10 +5,9 @@ class JavaWriter(name: String) : Writer(name, "java") {
         val langStrBuilder = StringBuilder()
 
         langStrBuilder.append("public class $name {\n    ")
-        langStrBuilder.append("public static final byte[] BYTES = new byte[]{\n        ")
+        langStrBuilder.append("public static final byte[] BYTES = new byte[] {\n        ")
         for ((i, byte) in bytes.withIndex()) {
-            val hexString = byte.toString(16).padStart(2, '0')
-            langStrBuilder.append("0x$hexString")
+            langStrBuilder.append("${byte.toString(10)}")
 
             if (i + 1 < bytes.size) {
                 langStrBuilder.append(",")
@@ -20,7 +19,7 @@ class JavaWriter(name: String) : Writer(name, "java") {
                 }
             }
         }
-        langStrBuilder.append("\n    }\n}\n")
+        langStrBuilder.append("\n    };\n}\n")
 
         return langStrBuilder.toString()
     }
