8d011473d5bf8645a5d75072cdad7c782ce2d72f
Hannes Braun <hannesbraun@mail.de>
2021-04-11 19:52:39 +0200

    KotlinWriter: convert literals to Bytes

--- src/commonMain/kotlin/writers/KotlinWriter.kt
+++ src/commonMain/kotlin/writers/KotlinWriter.kt
@@ -5,15 +5,15 @@ class KotlinWriter(name: String) : Writer(name, "kt") {
         val langStrBuilder = StringBuilder()
 
         langStrBuilder.append("object $name {\n    ")
-        langStrBuilder.append("val bytes = listOf<Byte>(\n        ")
+        langStrBuilder.append("val bytes = listOf(\n        ")
         for ((i, byte) in bytes.withIndex()) {
             val hexString = byte.toString(16).padStart(2, '0')
-            langStrBuilder.append("0x$hexString")
+            langStrBuilder.append("0x$hexString.toByte()")
 
             if (i + 1 < bytes.size) {
                 langStrBuilder.append(",")
 
-                if ((i + 1) % 12 == 0) {
+                if ((i + 1) % 5 == 0) {
                     langStrBuilder.append("\n        ")
                 } else {
                     langStrBuilder.append(" ")
