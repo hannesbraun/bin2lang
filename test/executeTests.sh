29a859375de45f92b1bf8e1b36472666514cd0b2
Hannes Braun <hannesbraun@mail.de>
2021-04-11 19:55:37 +0200

    Generate data structures automatically during test

--- test/executeTests.sh
+++ test/executeTests.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
+rm randData
+rm cOutFile
+rm javaOutFile
+
+bash randFile.sh
+
+BIN2LANG=../build/bin/macosX64/releaseExecutable/bin2lang.kexe
+
 # C
+$BIN2LANG c randData randomData
+rm c/randomData.h
+mv randomData.h c
 cd c
 make
 ./ctest
 cd -
 
 # Java
+$BIN2LANG java randData RandomData
+rm java/RandomData.java
+mv RandomData.java java
 cd java
 make
 java Main
