2f7324ec576fdbb56b2fcdbb3e70528afa579cbd
Hannes Braun <hannesbraun@mail.de>
2021-05-15 12:47:59 +0200

    Add missing newline to gpl notice

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -38,6 +38,7 @@ fun main(args: Array<String>) {
 
             You should have received a copy of the GNU General Public License
             along with this program.  If not, see <https://www.gnu.org/licenses/>.
+            
         """.trimIndent()
         )
     }
