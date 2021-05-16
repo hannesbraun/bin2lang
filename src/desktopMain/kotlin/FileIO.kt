acde7d644158e6897a4934ff60ca7f7b04914040
Hannes Braun <hannesbraun@mail.de>
2021-05-16 21:21:33 +0200

    Add file IO error handling

--- src/desktopMain/kotlin/FileIO.kt
+++ src/desktopMain/kotlin/FileIO.kt
@@ -2,14 +2,17 @@ import platform.posix.*
 import kotlinx.cinterop.*
 
 actual fun readFile(name: String): UByteArray {
-    val file = fopen(name, "rb")
-    fseek(file, 0, SEEK_END)
+    val file = fopen(name, "rb") ?: throw ReadException("Failed to open file $name")
+    var tmp = fseek(file, 0, SEEK_END)
+    if (tmp != 0) throw ReadException("fseek error")
     val fileSize = ftell(file).toInt()
-    fseek(file, 0, SEEK_SET)
+    tmp = fseek(file, 0, SEEK_SET)
+    if (tmp != 0) throw ReadException("fseek error")
     var input = ByteArray(0)
     memScoped {
         val buffer = allocArray<ByteVar>(fileSize)
-        fread(buffer, fileSize.toULong(), 1, file)
+        tmp = fread(buffer, fileSize.toULong(), 1, file).toInt()
+        if (tmp == 0) throw ReadException("fread error")
         input = buffer.readBytes(fileSize)
     }
     fclose(file)
@@ -17,7 +20,7 @@ actual fun readFile(name: String): UByteArray {
 }
 
 actual fun writeFile(name: String, data: String) {
-    val file = fopen(name, "w")
+    val file = fopen(name, "w") ?: throw WriteException("Failed to open file $name")
     memScoped { fprintf(file, data) }
     fclose(file)
 }
